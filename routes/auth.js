3a69a2653d9881c712818f14474c2947704040a7
TrygoDigital <ercapuchino@up.edu.ph>
2022-01-12 20:41:25 +0800

    feat: add register on auth - contains users collection

---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    console.log("on post")
+  try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log("pumasok sa una")
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+      console.log("nag error")
+    res.status(500).json(err)
+  }
+});
+
+module.exports = router;
